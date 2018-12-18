26735b7082164e0efcb70aeaa34067532d91aa98
Arbaz Khan <arbazkhan971@gmail.com>
2018-12-18 20:03:36 +0530

    lines detection and displaying done using hough transformation

--- lanes.py
+++ lanes.py
@@ -13,11 +13,21 @@ def roi(img):   #region of interest which is triangle here which may be dfferent
     cv2.fillPoly(mask , triangle,255)
     masked_image = cv2.bitwise_and(img,mask)
     return masked_image
+def display_lines(img,lines):
+    line_image = np.zeros_like(img)
+    if lines is not None:
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+    return line_image
 
 
-
-img = cv2.imread("images/road.jpg",0)
+img = cv2.imread("images/road.jpg")
+gray_img = cv2.imread("images/road.jpg",0)
 canny = edge_detection(img)
-roi=roi(canny)
-plt.imshow(roi)
+roi = roi(canny)
+lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
+line_image = display_lines(img, lines)
+combo_image = cv2.addWeighted(img, 0.8, line_image, 1, 1)
+plt.imshow(combo_image)
 plt.show()
