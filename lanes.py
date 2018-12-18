7cc55b48f1603065047d55f886e5cfd51f9e8430
Arbaz Khan <arbazkhan971@gmail.com>
2018-12-18 18:01:33 +0530

    region of interest is working fine now

--- lanes.py
+++ lanes.py
@@ -1,9 +1,22 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-img = cv2.imread("images/1258603797_preview_Road.jpg",0)
+def edge_detection(img):
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    canny = cv2.Canny(blur,50,150)
+    return canny
+def roi(img):   #region of interest which is triangle here which may be dfferent in your case depends on images which you take for finding edges using canny
+    bottom = img.shape[0]
+    triangle = np.array([[(220,bottom),(1020,bottom),(570,250)]])    #size of traingle with vertices
+    mask = np.zeros_like(img)                      #mask which is completely blacked
+    cv2.fillPoly(mask , triangle,255)
+    return mask
 
-blur = cv2.GaussianBlur(img,(5,5),0)
-canny = cv2.Canny(blur,50,150)
-cv2.imshow("Real line images",canny)
-cv2.waitKey(0)
-cv2.destroyAllwindows()
+
+
+img = cv2.imread("images/road.jpg",0)
+canny = edge_detection(img)
+roi=roi(canny)
+plt.imshow(roi)
+plt.show()
